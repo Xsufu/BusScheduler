09ca09eeff21d9e67527bf9b05a88bb9bfe13fe9
Xsufu <aluno.macarrao@gmail.com>
2022-06-06 14:54:00 +0700

    Создан Factory класс для ViewModel

--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.busschedule.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
 
 class BusScheduleViewModel (private val scheduleDao: ScheduleDao): ViewModel() {
     /**
-     * Получение всего распсиания
+     * Получение всего расписания
      */
     fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
 
@@ -16,4 +17,17 @@ class BusScheduleViewModel (private val scheduleDao: ScheduleDao): ViewModel() {
      * @param name название остановки
      */
     fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+}
+
+class BusScheduleViewModelFactory(
+    private val scheduleDao: ScheduleDao
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalAccessException("Unknown ViewModel class")
+    }
+
 }
