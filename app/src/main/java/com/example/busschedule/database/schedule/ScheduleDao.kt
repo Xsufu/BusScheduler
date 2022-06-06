49b42e79a81196b23016cffafd1d05dc9c99ffd3
Xsufu <aluno.macarrao@gmail.com>
2022-06-06 14:36:25 +0700

    Написаны запросы к БД

---
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -0,0 +1,23 @@
+package com.example.busschedule.database.schedule
+
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface ScheduleDao {
+    /**
+     * Запрос для получения всех данных из таблицы.
+     * Сортировка по времени прибытия по возрастанию
+     */
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): List<Schedule>
+
+    /**
+     * Запрос для получения расписания у конкретной остановки.
+     * Сортировка по времени прибытия по возрастанию
+     *
+     * @param stopName название остановки
+     */
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): List<Schedule>
+}
